ecd76e498a19a163fc039deac22afc7faf00ac7a
iamrushier <rushikeshsurve193@gmail.com>
2025-08-02 13:40:14 +0530

    Project init

---
+++ src/main/java/io/github/iamrushier/jobconnect_backend/JobconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.iamrushier.jobconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobconnectBackendApplication.class, args);
+	}
+
+}
